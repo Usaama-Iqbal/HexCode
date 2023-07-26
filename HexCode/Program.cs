7effd2de7ad3c009b9c2ad8c119cbf35079b9c8f
Usama Iqbal <uiqbal@cedrus.com>
2023-07-26 17:02:24 +0500

    feat: finds that the Value is hexa or not

---
+++ HexCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace HexCode
+{
+    class program
+    {
+        public static void Main(string[] args) 
+        {
+            string hc = "#CD5C5C";
+            Console.WriteLine("Hex Code: " + hc + " valid : " +test(hc));
+
+            hc = "#EAECEE";
+            Console.WriteLine("Hex Code: " + hc + " valid : " + test(hc));
+
+            hc = "#eaecee";
+            Console.WriteLine("Hex Code: " + hc + " valid : " + test(hc));
+
+            hc = "#CD5C58C";
+            Console.WriteLine("Hex Code: " + hc + " valid : " + test(hc));
+        }
+
+        public static bool test(string hc)
+        {
+
+            return Regex.IsMatch(hc, @"[#][0-9A-Fa-f]{6}\b");
+        }
+    }
+}
